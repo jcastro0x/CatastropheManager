6a065b29b8a736df4b58ec0490c5aebc11568994
jcastro0x <jcastro0x@gmail.com>
2021-08-03 04:25:28 +0200

    build.sh improved
    - auto delete output folder
    - use variables instead harcoded names

--- build.sh
+++ build.sh
@@ -1,19 +1,27 @@
 # Simple script to build the program
 # TODO: Create makefile instead this :)
 
-# We'll save in th(is folder the programs
-mkdir build
+BUILD_DIR="out"
+if [ -d $BUILD_DIR ]; then
+    echo "Remove old $BUILD_DIR folder"
+    rm -r $BUILD_DIR
+fi
+mkdir $BUILD_DIR
+
+
 
 #BOOST_LIBS = -lboost_date_time -lboost_program_options -lrt
 
 # Build Generator
 echo "(1/2) Building Generator..."
-g++ src/generator.cpp src/interpreter.cpp src/command.cpp include/common.cpp -Iinclude -lboost_date_time -lboost_program_options -lrt -o generator
-mv generator build/generator
+GENERATOR_NAME="generator"
+g++ src/generator.cpp src/interpreter.cpp src/command.cpp include/common.cpp -Iinclude -lboost_date_time -lboost_program_options -lrt -o $GENERATOR_NAME
+mv $GENERATOR_NAME $BUILD_DIR/$GENERATOR_NAME
 
 # Build Solver
 echo "(2/2) Building Solver..."
-g++ src/solver.cpp src/interpreter.cpp src/command.cpp include/common.cpp -Iinclude -lboost_date_time -lrt -o solver
-mv solver build/solver
+SOLVER_NAME="solver"
+g++ src/solver.cpp src/interpreter.cpp src/command.cpp include/common.cpp -Iinclude -lboost_date_time -lrt -o $SOLVER_NAME
+mv $SOLVER_NAME $BUILD_DIR/$SOLVER_NAME
 
 echo "Done!"
