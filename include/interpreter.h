a0947f13a8bc427564c9eebcc422c0a22a7757e9
jcastro0x <jcastro0x@gmail.com>
2021-08-04 15:12:44 +0200

    Add license

--- include/interpreter.h
+++ include/interpreter.h
@@ -1,3 +1,23 @@
+// Copyright (c) 2020-2021 Javier Castro - jcastro0x@gmail.com.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+
 #pragma once
 
 #include <commands/command.h>
@@ -20,4 +40,23 @@ private:
     MemoryManager                           m_memoryManager {       };
     Options                                 m_options                ;
     bool                                    m_bRunning      { false };
+
+
+    inline static const char* generator_title = R"(
+run as:        
+___|                                 |                
+|       _ \  __ \    _ \   __|  _` |  __|   _ \    __| 
+|   |   __/  |   |   __/  |    (   |  |    (   |  |    
+\____| \___| _|  _| \___| _|   \__,_| \__| \___/  _|  
+            ~catastrophe manager suite~          
+    )";
+
+    inline static const char* solver_title = R"(
+run as:
+  ___|          |                    
+\___ \    _ \   | \ \   /  _ \   __| 
+      |  (   |  |  \ \ /   __/  |    
+_____/  \___/  _|   \_/  \___| _|    
+    ~catastrophe manager suite~
+    )";    
 };
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,3 +1,23 @@
+// Copyright (c) 2020-2021 Javier Castro - jcastro0x@gmail.com.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+// SOFTWARE.
+
 #include <interpreter.h>
 #include <commands.h>
 
@@ -12,16 +32,19 @@ Interpreter::Interpreter(int argc, char** argv)
     m_commands.emplace_back(std::make_unique<CmdHelp>());
     m_commands.emplace_back(std::make_unique<CmdStatus>());
 
+    std::cout << "\033[2J"; // clear screen
     if(m_options.get_runAs() == EMode::Generator)
     {
-        std::cout << "Run As Generator\n";
+        std::cout << generator_title << std::endl;
         m_commands.emplace_back(std::make_unique<CmdGenerate>());
     }
     else
     {
-        std::cout << "Run As Solver\n";
+        std::cout << solver_title << std::endl;
         m_commands.emplace_back(std::make_unique<CmdSolve>());
     }
+
+    std::cout << "\033[32mWrite 'help' to print help\033[0m\n";
 }
 
 void Interpreter::run()
